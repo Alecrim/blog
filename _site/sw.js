472b812905844d4cbee7428d55db528668551595
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2021-08-21 22:34:17 -0300

    Do initial adjustments

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210822.0913';
+const cacheName = 'chirpy-20210821.2233';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
