0991d080b2e407f2f5c82ba46101fa491de92736
Vanderlei Martinelli <vanderlei.martinelli@gmail.com>
2021-08-21 23:22:52 -0300

    Update contact info

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210821.2233';
+const cacheName = 'chirpy-20210821.2320';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
